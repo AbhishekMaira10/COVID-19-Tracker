04ccd2ca07d96dc2943b266ee902ffa5d7bcb583
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-05-05 20:45:06 +0530

    updated main, created IntroPage

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatefulWidget {
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
