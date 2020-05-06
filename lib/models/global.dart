a19f76fee369e00babda804a77c0034039b2b1cd
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-05-06 20:57:11 +0530

    completed IntroPage method

---
+++ lib/models/global.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color backgroundColor = Color(0XFFefedf2);
+  static final Color mainColor = Colors.indigoAccent;
+}
--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,3 +1,5 @@
+import 'package:covid_19_tracker/models/global.dart';
+import 'package:covid_19_tracker/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class IntroPage extends StatefulWidget {
@@ -8,6 +10,114 @@ class IntroPage extends StatefulWidget {
 class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              AppColors.mainColor,
+              AppColors.mainColor.withOpacity(.5),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Stack(
+          children: <Widget>[
+            _buildHeader(),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: MediaQuery.of(context).size.width * .75,
+                child: Image.asset("assets/images/virus.png"),
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * .25,
+              right: 25,
+              child: Container(
+                width: MediaQuery.of(context).size.width * .4,
+                child: Image.asset("assets/images/person.png"),
+              ),
+            ),
+            _buildFooter(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding _buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 50),
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: Image.asset("assets/images/logo.png"),
+      ),
+    );
+  }
+
+  Widget _buildFooter(BuildContext context) {
+    return Positioned(
+      bottom: 50,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Nothing in life is to be feared, it is only to be understood.\n",
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.deepPurple,
+                height: 1.5,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 25),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => HomePage(),
+                  ),
+                );
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(1, 1),
+                      spreadRadius: 1,
+                      blurRadius: 3,
+                    )
+                  ],
+                ),
+                width: MediaQuery.of(context).size.width * .85,
+                height: 60,
+                child: Center(
+                  child: Text(
+                    "GET STARTED",
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
