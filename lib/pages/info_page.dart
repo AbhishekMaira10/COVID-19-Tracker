9de605b1c5ffe4010ea1a972f8db1d9e8a3788ab
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-05-07 02:29:02 +0530

    added custom header to info_page

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -1,8 +1,49 @@
+import 'package:covid_19_tracker/models/custom_header.dart';
 import 'package:flutter/material.dart';
 
-class InfoPage extends StatelessWidget {
+class InfoPage extends StatefulWidget {
+  @override
+  _InfoPageState createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+  final controller = ScrollController();
+  double offset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void onScroll() {
+    setState(() {
+      offset = (controller.hasClients) ? controller.offset : 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        controller: controller,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              MyHeader(
+                image: "assets/icons/coronadr.svg",
+                textTop: "Get to know",
+                textBottom: "About Covid-19.",
+                offset: offset,
+              ),
+            ]),
+      ),
+    );
   }
 }
