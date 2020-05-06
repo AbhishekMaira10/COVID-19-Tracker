ae1906977c4c9cd02e02889ecb37271fbca14b9c
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-05-07 01:48:26 +0530

    added code for custom header

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,49 @@
+import 'package:covid_19_tracker/models/custom_header.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = ScrollController();
+  double offset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void onScroll() {
+    setState(() {
+      offset = (controller.hasClients) ? controller.offset : 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        controller: controller,
+        child: Column(
+          children: <Widget>[
+            MyHeader(
+              image: "assets/icons/Drcorona.svg",
+              textTop: "All you need to do",
+              textBottom: "is to stay at home!!!",
+              offset: offset,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/pages/info_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class InfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
