db0c312d0d770c59cb11499b49a78912d07efe8d
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-05-08 00:02:24 +0530

    added regional countrydata feature

--- lib/pages/country_page.dart
+++ lib/pages/country_page.dart
@@ -1,8 +1,122 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class CountryPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+class CountryPage extends StatefulWidget {
+  @override
+  _CountryPageState createState() => _CountryPageState();
+}
+
+class _CountryPageState extends State<CountryPage> {
+  List countryData;
+
+  fetchCountryData() async {
+    http.Response response =
+        await http.get('https://corona.lmao.ninja/v2/countries');
+    setState(() {
+      countryData = json.decode(response.body);
+    });
+  }
+
+  @override
+  void initState() {
+    fetchCountryData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Country Stats')),
+        backgroundColor: Color(0xff202c3b),
+      ),
+      body: countryData == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 130,
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey[100],
+                        blurRadius: 10,
+                        offset: Offset(0, 10)),
+                  ]),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              countryData[index]['country'],
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Image.network(
+                              countryData[index]['countryInfo']['flag'],
+                              height: 50,
+                              width: 60,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                          child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'CONFIRMED:' +
+                                  NumberFormat('#,###,###,###')
+                                      .format(countryData[index]['cases'])
+                                      .toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orange),
+                            ),
+                            Text(
+                              'ACTIVE:' +
+                                  NumberFormat('#,###,###,###')
+                                      .format(countryData[index]['active'])
+                                      .toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.teal),
+                            ),
+                            Text(
+                              'RECOVERED:' +
+                                  NumberFormat('#,###,###,###')
+                                      .format(countryData[index]['cases'])
+                                      .toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.green),
+                            ),
+                            Text(
+                              'DEATHS:' +
+                                  NumberFormat('#,###,###,###')
+                                      .format(countryData[index]['cases'])
+                                      .toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red),
+                            ),
+                          ],
+                        ),
+                      ))
+                    ],
+                  ),
+                );
+              },
+              itemCount: countryData == null ? 0 : countryData.length,
+            ),
+    );
   }
 }
