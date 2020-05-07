baaf50acc6d21600ea076793a6612f6cfc331f1e
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-05-07 20:51:39 +0530

    added donaton link

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,6 +6,7 @@ import 'package:covid_19_tracker/panels/most_affected_countries.dart';
 import 'package:covid_19_tracker/panels/worldwide_panel.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -82,6 +83,26 @@ class _HomePageState extends State<HomePage> {
                     'Worldwide',
                     style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                   ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      launch('https://covid19responsefund.org/');
+                    },
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.redAccent,
+                            borderRadius: BorderRadius.circular(15)),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'Donate',
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        )),
+                  ),
                   GestureDetector(
                     onTap: () {
                       Navigator.push(
@@ -123,6 +144,17 @@ class _HomePageState extends State<HomePage> {
                 : MostAffectedPanel(
                     countryData: countryData,
                   ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+                child: Text(
+              'WE ARE TOGETHER IN THIS FIGHT',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            )),
+            SizedBox(
+              height: 50,
+            )
           ],
         ),
       ),
